c9501d6c87dfbb58bc90adfd09c4db15b2771d7f
peter Nieuwenhuyse <peter.nieuwenhuyse@gmail.com>
2018-11-09 16:10:05 +0100

    prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,9 @@ import {IMqttServiceOptions} from 'ngx-mqtt';
 export const environment = {
   production: true
 };
+
 export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
-    hostname: 'ws://whosa.be',
+    hostname: '//whosa.be',
     port: 8080,
     path: ''
-}
+};
